05574fcc093be008f6956a226808e2b421c1aa2b
kevinlm2282 <55009031+kevinlm2282@users.noreply.github.com>
2025-05-18 13:40:24 -0400

    fix:problema de cors solucionado

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 // var path = require('path');
 // var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
@@ -7,7 +8,7 @@ require('dotenv').config()
 // var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors())
 // app.use(logger('dev'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
